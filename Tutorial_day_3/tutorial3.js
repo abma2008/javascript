74b656483c974ef302281e7c5ddde41d57076fd5
Ahmed Almashani <ak991983@gmail.com>
2023-01-10 02:16:50 +0400

    Adding some additional Features to Tutorial3 files

--- Tutorial_day_3/tutorial3.js
+++ Tutorial_day_3/tutorial3.js
@@ -7,6 +7,14 @@ to see how far you can reach in this tutorial.
 Best of luck
 */
 
+// This is an addition feature where we can display the time and make it work:
+let displaytime =(()=>{
+    let display = document.getElementById(`displaytime`);
+    display.innerHTML = new Date();
+});
+
+setInterval(displaytime, 1000);
+
 
 const number1 = document.getElementById(`number1`);
 const number2 = document.getElementById(`number2`);
@@ -15,14 +23,17 @@ const button = document.getElementById(`submit`);
 
 button.addEventListener(`click`, function(e){
     e.preventDefault();
+    // Checking the two values to avoid errors:
     // calculate the two numbers above:
     try {
         let result = parseFloat(number1.value) + parseFloat(number2.value);
-        alert(`you will be viewing the results after clicking ok`);
-        alert(result)
+        alert(`The Additional Result = ${result}`);
+        
         
     }
     catch {
         alert(`Something went wrong, please try again`)
-    }
-})
+    
+}
+    
+});
