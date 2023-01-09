65316d37de03e56b54b304223e926d19ff6f1c7c
Ahmed Almashani <ak991983@gmail.com>
2023-01-09 13:54:50 +0400

    adding Tutorial_day_2 Folder

---
+++ Tutorial_day_2/tutorial2.js
@@ -0,0 +1,9 @@
+/*
+I will be adding some code here and it should be basic and that is because
+I want to learn the basics of JavaScript and also, get more familiar with 
+git and github. 
+please make sure to keep working on javascript and also git and github
+to see how far you can reach in this tutorial.
+Best of luck
+*/
+
