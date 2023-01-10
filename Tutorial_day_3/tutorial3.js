e75c67d716189f61694249dff7e72501cb4bbfa1
Ahmed Almashani <ak991983@gmail.com>
2023-01-10 05:02:46 +0400

    Modifying the content of Tutorial3 Folder

--- Tutorial_day_3/tutorial3.js
+++ Tutorial_day_3/tutorial3.js
@@ -6,7 +6,8 @@ please make sure to keep working on javascript and also git and github
 to see how far you can reach in this tutorial.
 Best of luck
 */
-
+$(document).ready(function(){
+    
 // This is an addition feature where we can display the time and make it work:
 let displaytime =(()=>{
     let display = document.getElementById(`displaytime`);
@@ -18,26 +19,35 @@ setInterval(displaytime, 1000);
 
 const number1 = document.getElementById(`number1`);
 const number2 = document.getElementById(`number2`);
+// Setting the values of numbers to parseFloat:
 
 const button = document.getElementById(`submit`);
 
 button.addEventListener(`click`, function(e){
     e.preventDefault();
-    // Checking the two values to avoid errors:
-    
-            // calculate the two numbers above:
+    if(isNaN(number1.value) || isNaN(number2.value))
+    {
+        alert(`something went wrong, cannot calculate empty values `)
+    }
+    else
+    {
         try {
-            let result = parseFloat(number1.value) + parseFloat(number2.value);
+            let num1 = parseFloat(number1.value);
+            let num2 = parseFloat(number2.value);
+            let result = num1+ num2;
             alert(`The Additional Result = ${result}`);
-            
+                
+                
+        }catch {
+            alert(`Something went wrong, please try again`)
             
         }
-        catch {
-            alert(`Something went wrong, please try again`)
         
     }
-    console.log(typeof(number1.value))
-    console.log(typeof(number2.value))
     
-    
+    // this is the end of 
 });
+
+
+// This is the end of Jquery 
+})
